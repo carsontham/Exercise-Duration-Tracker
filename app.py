d22bf27aa68251cf4702b68d3525aceec5757c62
carsontham <carsontham@outlook.com>
2023-12-14 00:08:44 +0800

    edited config

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ CORS(app)
 
 import auth
 
-app.secret_key = '\xfd{H\xe5<\x95\xf9\xe3\x96.5\xd1\x01O<!\xd5\xa2\xa0\x9fR"\xa1\xa8'
-
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
 def login():
